f1733452070c5374c804ac2c8ca9923aee42b428
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-10-23 15:20:09 +0100

    create DeliveryRepository  and TourRepository WarehouseRepository

---
+++ src/main/java/com/example/Livraison/dao/Repository/TourRepository.java
@@ -0,0 +1,7 @@
+package com.example.Livraison.dao.Repository;
+
+import com.example.Livraison.model.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourRepository extends JpaRepository<Warehouse, Long> {
+}
